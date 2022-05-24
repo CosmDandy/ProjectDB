27a573174e2a4c269ef9088d80d074ec5d2249a6
CosmDandy <tkondrashin@icloud.com>
2022-05-24 14:02:50 +0300

    add deleted

--- main.php
+++ main.php
@@ -1,6 +1,10 @@
 <?php
 require_once("Connections/project_con.php");
 
+if (!$_SESSION['auth'] and ($_SESSION['status'] < 1)) {
+    header('Location: ' . 'login.php');
+}
+
 $user = $_SESSION['user_id'][0];
 $select_note = mysqli_query($link, "SELECT * FROM notes WHERE user_id = '$user' ORDER BY created DESC ");
 $user_search = str_replace(',', ' ', $_GET['user_search']);
@@ -55,56 +59,51 @@ if (!empty($where_list)) {
 
 <!-- Main Content -->
 <div class="content_wrapper">
-    <?php if ($_SESSION['auth'] and ($_SESSION['status'] >= 1)) { ?>
+    <div class="content">
+        <div class="search">
+            <form name="search" method="get" action="">
+                <label for="user_search">
+                    <input type="text" name="user_search" id="user_search">
+                </label>
+                <button type="submit">
+                    <img alt="#" src="photos/search.png">
+                </button>
+            </form>
+        </div>
+    </div>
+    <?php if ($final_search_words[0] != "") {
+        while ($res_array = mysqli_fetch_array($res_query)) { ?>
+            <div class="content">
+                <div class="block">
+                    <div class="">
+                        <h2><?php echo $res_array['id'], "<br>"; ?></h2>
+                    </div>
+                    <div class="">
+                        <h2><?php echo $res_array['title'], "<br>"; ?></h2>
+                    </div>
+                    <div class="">
+                        <h2><?php echo $res_array['article'], "<br>", "<hr>", "<br>"; ?></h2>
+                    </div>
+                </div>
+            </div>
+        <?php }
+    } else { ?>
+    <?php while ($note = mysqli_fetch_array($select_note)) {
+        if (!($note['deleted'])){?>
         <div class="content">
-            <div class="search">
-                <form name="search" method="get" action="">
-                    <label for="user_search">
-                        <input type="text" name="user_search" id="user_search">
-                    </label>
-                    <button type="submit">
-                        <img alt="#" src="photos/search.png">
-                    </button>
-                </form>
+            <div class="block">
+                <div class="">
+                    <h2><?php echo $note['title']; ?></h2>
+                </div>
+                <div class="">
+                    <p>Дата: <?php echo $note['created']; ?></p>
+                </div>
+                <div class="">
+                    <p><?php echo $note['article']; ?></p>
+                </div>
             </div>
         </div>
-        <?php if ($final_search_words[0] != "") {
-            while ($res_array = mysqli_fetch_array($res_query)) { ?>
-                <div class="content">
-                    <div class="block">
-                        <div class="">
-                            <h2><?php echo $res_array['id'], "<br>"; ?></h2>
-                        </div>
-                        <div class="">
-                            <h2><?php echo $res_array['title'], "<br>"; ?></h2>
-                        </div>
-                        <div class="">
-                            <h2><?php echo $res_array['article'], "<br>", "<hr>", "<br>"; ?></h2>
-                        </div>
-                    </div>
-                </div>
-            <?php }
-        } else { ?>
-            <?php while ($note = mysqli_fetch_array($select_note)) { ?>
-                <div class="content">
-                    <div class="block">
-                        <div class="">
-                            <h2><?php echo $note['title']; ?></h2>
-                        </div>
-                        <div class="">
-                            <p>Дата: <?php echo $note['created']; ?></p>
-                        </div>
-                        <div class="">
-                            <p><?php echo $note['article']; ?></p>
-                        </div>
-                    </div>
-                </div>
-            <?php }
-        }
-    } else {
-        header('Location: ' . 'login.php');
-    }
-    ?>
+    <?php } } }?>
 </div>
 </body>
 </html>
