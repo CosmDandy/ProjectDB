5d99d6cd604c0818df3607a5bbf5694666ca1b14
CosmDandy <tkondrashin@icloud.com>
2022-06-12 12:25:43 +0300

    +

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,19 +1,6 @@
-function show_hide_password_1(target) {
+function show_hide_password(target) {
     var ph = document.getElementById("pass1");
     var input = document.getElementById('password1');
-    if (input.getAttribute('type') == 'password') {
-        input.setAttribute('type', 'text');
-        ph.src = ph.src.replace("Photos/show.png", "Photos/hide.png");
-    } else {
-        input.setAttribute('type', 'password');
-        ph.src = ph.src.replace("Photos/hide.png", "Photos/show.png");
-    }
-    return false;
-}
-
-function show_hide_password_2(target) {
-    var ph = document.getElementById("pass2");
-    var input = document.getElementById('password2');
     if (input.getAttribute('type') == 'password') {
         input.setAttribute('type', 'text');
         ph.src = ph.src.replace("Photos/show.png", "Photos/hide.png");
